b9f7e26c27ae832743d4c41107d77b4a3c1852f6
Dom <dominickb613@gmail.com>
2019-11-25 17:39:06 -0500

    Add EasyOpenCV to project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
@@ -1,49 +1,82 @@
-    package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.robotcontroller.external.samples;
 
-public class Detector {
+import com.google.gson.internal.$Gson$Preconditions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
-    // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
-    // first.
-    initVuforia();
+public class Detector extends LinearOpMode {
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";
+    private static final String VUFORIA_KEY =
+            "ARjw1eT/////AAABmTT9RZG8ZEFjviGE0IUocPIFdi7CCM1gAaiqVAQ1ys4VFImCtEPPtXDdL2pT3U3lejCSeEFD02QN5EWRHrSCvorLvhWonQqGVzsoJS7VMfIkRVvc81cJ9C4njQfYw/B8JrmTp8UIarY42jfMPpXU3Bjq04E8XOG6d+NizKPooRsb4zHlUheenmroS2FeusBkGwsKtWd328bj+R8Jf0AJcmNCZQuDAeJjXZvhrDH/1G11qxz6w1A4EEvlQ8mgeupdnLZkggUYOF8apgpxA7YMkZ4pCxljLWhH0GQ4UAatJWSBbO+9LHn5ShwfH3iOOBrZakD8kEL4x1zQlV2m2sTWg3okrVqmID2hotfGTPmzUOmK";
 
-    if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
-        initTfod();
-    } else {
-        telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-    }
+    private VuforiaLocalizer vuforia;
 
-    /**
-     * Activate TensorFlow Object Detection before we wait for the start command.
-     * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-     **/
-    if (tfod != null) {
-        tfod.activate();
-    }
+    private TFObjectDetector tfod;
 
-        // getUpdatedRecognitions() will return null if no new information is available since
-        // the last time that call was made.
-    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-    if (updatedRecognitions != null) {
-        telemetry.addData("# Object Detected", updatedRecognitions.size());
+    @Override
+    public void runOpMode() {
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
 
-        // step through the list of recognitions and display boundary info.
-        int i = 0;
-        for (Recognition recognition : updatedRecognitions) {
-            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                    recognition.getLeft(), recognition.getTop());
-            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                    recognition.getRight(), recognition.getBottom());
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
         }
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
-    }
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                            skyHeight = recognition.getTop() = recognition.getBottom();
+                            skyWidthL = recognition.getLeft();
+                            skyWidthR = recognition.getRight();
+                        }
+                        telemetry.update();
+                    }
+                }
+            }
+        }
 
         if (tfod != null) {
             tfod.shutdown();
         }
-
     }
-
 
     /**
      * Initialize the Vuforia localization engine.
