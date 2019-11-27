73824fce6d7b7fd282c6ab78a0e2fddca831f06e
ver09934 <ianvernooy@gmail.com>
2019-11-27 15:07:39 -0500

    Add IMU test auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIMUAuton.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name = "Test IMU Auton")
+public class TestIMUAuton extends LinearOpMode {
+
+    private ElapsedTime runtime;
+
+    BNO055IMU imu;
+    Orientation lastAngles = new Orientation();
+
+    double globalAngle;
+    double correction;
+
+    @Override
+    public void runOpMode() {
+
+        // --- Init Section ---
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        runtime = new ElapsedTime();
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+
+        telemetry.addData("Status", "Calibrating IMU");
+        telemetry.update();
+
+        while (!isStopRequested() && !imu.isGyroCalibrated()) {
+            sleep(50);
+            idle();
+        }
+
+        while (!this.isStarted()) {
+            telemetry.addData("Status", "Waiting for start");
+            telemetry.addData("Runtime", runtime.toString());
+            telemetry.addData("IMU calibration status", imu.getCalibrationStatus().toString());
+            telemetry.update();
+        }
+
+        // --- Run Section ----
+
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Runtime", runtime.toString());
+            telemetry.addLine(angles.toString());
+            telemetry.update();
+        }
+    }
+}
