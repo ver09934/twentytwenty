5ff51a6ccf4806d966dc78dbf0619bb4ffee81b4
ver09934 <ianvernooy@gmail.com>
2019-12-01 09:49:10 -0500

    Move everything into one class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneAuton.java
@@ -1,78 +1,231 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 @Autonomous(name = "Skystone Auton")
 public class SkystoneAuton extends LinearOpMode {
 
     private ElapsedTime runtime;
+    BNO055IMU imu;
 
     @Override
     public void runOpMode() {
 
-        // --- Init Section ---
+        // ----- INIT SECTION -----
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         runtime = new ElapsedTime();
 
+        lfMotor = hardwareMap.dcMotor.get("lfMotor");
+        rfMotor = hardwareMap.dcMotor.get("rfMotor");
+        lbMotor = hardwareMap.dcMotor.get("lbMotor");
+        rbMotor = hardwareMap.dcMotor.get("rbMotor");
+        setAllRunModes(DcMotor.RunMode.RUN_USING_ENCODER);
+        setAllZeroPowerBehaviors(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        while (!isStopRequested() && !imu.isGyroCalibrated()) {
+            telemetry.addData("Status", "Calibrating IMU");
+            telemetry.update();
+            idle();
+        }
+
         while (!this.isStarted()) {
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
-        }
-        // waitForStart();
-
-        // --- Run Section ---
-
-        runtime.reset();
-
-        AutonDrivetrain autonDrivetrain = new AutonDrivetrain(hardwareMap, telemetry);
-
-        autonDrivetrain.moveCardinal(0.5, 304.8, 0);
-        // autonDrivetrain.moveDistance(0.5, 243.84, 60);
-
-        /*
-        for (int angle : new int[]{0, 90, 180, 270}) {
-            autonDrivetrain.moveCardinal(0.5, 200, angle);
-            sleep(1000);
+            telemetry.addData("Status", "Waiting for start");
+            telemetry.addData("Runtime", runtime.toString());
+            telemetry.addData("IMU calibration status", imu.getCalibrationStatus().toString());
         }
 
-        sleep(3000);
-
-        for (int angle = 0; angle < 360; angle += 45) {
-            autonDrivetrain.moveDistance(0.5, 150, angle);
-            sleep(1000);
-        }
-
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Run time: " + runtime.toString());
-            telemetry.update();
-        }
-         */
+        // ----- RUN SECTION -----
 
     }
 
-    // --- Other Methods ---
+    // ----- DRIVING STUFF -----
 
-    // TODO: Implement
-    public static void moveAlongWall(double walldist, double stopdist) {
+    public static final double GEAR_RATIO = 1; // output/input teeth
+    public static final int TICKS_PER_MOTOR_ROTATION = 1120;
+    public static final int TICKS_PER_WHEEL_ROTATION = (int)((TICKS_PER_MOTOR_ROTATION * GEAR_RATIO) + 0.5);
+    public static final double WHEEL_DIAMETER = 10.16; // [cm]
+    public static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER; // [cm]
+    public static final double DISTANCE_PER_TICK = WHEEL_CIRCUMFERENCE / TICKS_PER_WHEEL_ROTATION;
 
-        /*
-        if (percentWideWallDistanceDifference > tolerance) {
-            setSidewaysWallDistance(targetSideWallDistance)
-        }
-        then, implement move towards wall
-         */
-
-        /*
-        Ideas:
-        if walldist is way off; then
-            strafe to get walldist approx
-        fi
-        PID loop to stopdist
-         */
+    public static int distanceToEncoderTicks(double distance) {
+        return (int)((distance / DISTANCE_PER_TICK) + 0.5);
     }
+
+    public void moveCardinal(double power, double distance, int direction) {
+
+        resetAllEncoders();
+
+        int[] motorDirections = {1, 1, 1, 1};
+
+        if (direction == 0) {
+            motorDirections[0] = -1;
+            motorDirections[1] = -1;
+        }
+        else if (direction == 90) {
+            motorDirections[0] = -1;
+            motorDirections[2] = -1;
+        }
+        else if (direction == 180) {
+            motorDirections[2] = -1;
+            motorDirections[3] = -1;
+        }
+        else if (direction == 270) {
+            motorDirections[1] = -1;
+            motorDirections[3] = -1;
+        }
+        else {
+            // This is bad, I know
+            throw new RuntimeException("Direction not multiple of 90 between 0 and 270, inclusive");
+        }
+
+        if (direction == 0 || direction == 180) {
+            // Empirically determined values for strafing sideways
+            distance *= ((304.8) / (304.8 - 19));
+        }
+
+        // TODO: See if needed
+        double maxPower = Math.sqrt(2);
+
+        lfMotor.setPower(power * motorDirections[0] / maxPower);
+        rfMotor.setPower(power * motorDirections[1] / maxPower);
+        lbMotor.setPower(power * motorDirections[2] / maxPower);
+        rbMotor.setPower(power * motorDirections[3] / maxPower);
+
+        double targetTicks = distanceToEncoderTicks(distance);
+
+        int averageMotorTicks = 0;
+
+        while (averageMotorTicks < targetTicks) {
+            int lft = Math.abs(lfMotor.getCurrentPosition());
+            int rft = Math.abs(rfMotor.getCurrentPosition());
+            int lbt = Math.abs(lbMotor.getCurrentPosition());
+            int rbt = Math.abs(rbMotor.getCurrentPosition());
+            averageMotorTicks = (lft + rft + lbt + rbt) / 4;
+
+            telemetry.addLine("Target Ticks: " + targetTicks);
+            telemetry.addLine("LF Actual: " + lft);
+            telemetry.addLine("RF Actual: " + rft);
+            telemetry.addLine("LB Actual: " + lbt);
+            telemetry.addLine("RB Actual: " + rbt);
+            telemetry.update();
+        }
+
+        setAllMotorPowers(0);
+    }
+
+    public void turnDegrees(double turnPower, double angleDelta) {
+
+        assert Math.abs(angleDelta) <= 180;
+
+        double startAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        startAngle = startAngle < 0 ? startAngle + 360 : startAngle;
+
+        double targetAngle = startAngle + angleDelta;
+
+        if (targetAngle > 360) {
+            targetAngle = targetAngle % 360;
+        }
+        else if (targetAngle < 0) {
+            targetAngle = targetAngle % 360;
+            targetAngle += 360;
+        }
+
+        turnPower = Math.copySign(turnPower, angleDelta);
+
+        lfMotor.setPower(turnPower);
+        rfMotor.setPower(turnPower);
+        lbMotor.setPower(turnPower);
+        rbMotor.setPower(turnPower);
+
+        double currentAngle = startAngle;
+
+        if (angleDelta > 0 && targetAngle < startAngle) {
+            while (currentAngle >= startAngle || currentAngle < targetAngle) {
+                currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+                currentAngle = currentAngle < 0 ? currentAngle + 360 : currentAngle;
+                telemetry.addLine("Delta: " + angleDelta + " Target: " + targetAngle + " Current: " + currentAngle);
+                telemetry.update();
+            }
+        }
+        else if (angleDelta < 0 && targetAngle > startAngle) {
+            while (currentAngle <= startAngle || currentAngle > targetAngle) {
+                currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+                currentAngle = currentAngle < 0 ? currentAngle + 360 : currentAngle;
+                telemetry.addLine("Delta: " + angleDelta + " Target: " + targetAngle + " Current: " + currentAngle);
+                telemetry.update();
+            }
+        }
+        else if (angleDelta > 0) {
+            while (currentAngle < targetAngle) {
+                currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+                currentAngle = currentAngle < 0 ? currentAngle + 360 : currentAngle;
+                telemetry.addLine("Delta: " + angleDelta + " Target: " + targetAngle + " Current: " + currentAngle);
+                telemetry.update();
+            }
+        }
+        else {
+            while (currentAngle > targetAngle) {
+                currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+                currentAngle = currentAngle < 0 ? currentAngle + 360 : currentAngle;
+                telemetry.addLine("Delta: " + angleDelta + " Target: " + targetAngle + " Current: " + currentAngle);
+                telemetry.update();
+            }
+        }
+
+        setAllMotorPowers(0);
+    }
+
+    // ----- MOTOR STUFF -----
+
+    private DcMotor lfMotor;
+    private DcMotor rfMotor;
+    private DcMotor lbMotor;
+    private DcMotor rbMotor;
+
+    Telemetry telemetry;
+
+    public void setAllRunModes(DcMotor.RunMode runMode) {
+        lfMotor.setMode(runMode);
+        rfMotor.setMode(runMode);
+        lbMotor.setMode(runMode);
+        rbMotor.setMode(runMode);
+    }
+
+    public void setAllZeroPowerBehaviors(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+        lfMotor.setZeroPowerBehavior(zeroPowerBehavior);
+        rfMotor.setZeroPowerBehavior(zeroPowerBehavior);
+        lbMotor.setZeroPowerBehavior(zeroPowerBehavior);
+        rbMotor.setZeroPowerBehavior(zeroPowerBehavior);
+    }
+
+    public void resetAllEncoders() {
+        setAllRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setAllRunModes(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void setAllMotorPowers(double power) {
+        lfMotor.setPower(power);
+        rfMotor.setPower(power);
+        lbMotor.setPower(power);
+        rbMotor.setPower(power);
+    }
+
 }
