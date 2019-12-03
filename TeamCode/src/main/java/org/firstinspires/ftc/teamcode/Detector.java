916c7fce28ebdcf6b748d59f4a0c69d6c8aac3b2
Dom <dominickb613@gmail.com>
2019-12-03 17:43:08 -0500

    Add more EasyOpenCV methods and begin adding more structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Detector.java
@@ -2,109 +2,60 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.google.gson.internal.$Gson$Preconditions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import java.util.List;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.core.*;
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
-public class Detector extends LinearOpMode {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
+
+public class Detector extends OpenCvPipeline {
+
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";
     private static final String LABEL_SECOND_ELEMENT = "Skystone";
-    private static final String VUFORIA_KEY =
-            "ARjw1eT/////AAABmTT9RZG8ZEFjviGE0IUocPIFdi7CCM1gAaiqVAQ1ys4VFImCtEPPtXDdL2pT3U3lejCSeEFD02QN5EWRHrSCvorLvhWonQqGVzsoJS7VMfIkRVvc81cJ9C4njQfYw/B8JrmTp8UIarY42jfMPpXU3Bjq04E8XOG6d+NizKPooRsb4zHlUheenmroS2FeusBkGwsKtWd328bj+R8Jf0AJcmNCZQuDAeJjXZvhrDH/1G11qxz6w1A4EEvlQ8mgeupdnLZkggUYOF8apgpxA7YMkZ4pCxljLWhH0GQ4UAatJWSBbO+9LHn5ShwfH3iOOBrZakD8kEL4x1zQlV2m2sTWg3okrVqmID2hotfGTPmzUOmK";
 
-    private VuforiaLocalizer vuforia;
+    private Mat result = new Mat();
+    private List<Mat> recognitions = new ArrayList<>();
 
-    private TFObjectDetector tfod;
+    private int position;
+    private Telemetry telemetry;
+    private boolean showUI;
+
+
+    Detector(Telemetry telemetry, boolean showUI) {
+        super();
+        this.telemetry = telemetry;
+        this.showUI = showUI;
+    }
+
+    public int getPosition() { return position; }
 
     @Override
-    public void runOpMode() {
-        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
-        // first.
-        initVuforia();
+    public Mat processFrame(Mat input) {
 
-        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
-            initTfod();
-        } else {
-            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-        }
+        telemetry.addLine("---Detection Algorithm Begins---");
+        System.out.println("---Detection Algorithm Begins---");
 
-        /**
-         * Activate TensorFlow Object Detection before we wait for the start command.
-         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-         **/
-        if (tfod != null) {
-            tfod.activate();
-        }
+        Size imageSize = input.size();
+        double w = imageSize.width;
+        double h = imageSize.height;
 
-        /** Wait for the game to begin */
-        telemetry.addData(">", "Press Play to start op mode");
-        telemetry.update();
-        waitForStart();
+        Net model = Dnn.readNetFromTensorflow;
 
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    // the last time that call was made.
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+        model.setInput(input);
+        result = model.forward();
 
-                        // step through the list of recognitions and display boundary info.
-                        int i = 0;
-                        for (Recognition recognition : updatedRecognitions) {
-                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                    recognition.getLeft(), recognition.getTop());
-                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                    recognition.getRight(), recognition.getBottom());
-                            skyHeight = recognition.getTop() = recognition.getBottom();
-                            skyWidthL = recognition.getLeft();
-                            skyWidthR = recognition.getRight();
-                        }
-                        telemetry.update();
-                    }
-                }
-            }
-        }
-
-        if (tfod != null) {
-            tfod.shutdown();
-        }
-    }
-
-    /**
-     * Initialize the Vuforia localization engine.
-     */
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = CameraDirection.BACK;
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
-    }
-
-    /**
-     * Initialize the TensorFlow Object Detection engine.
-     */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minimumConfidence = 0.4;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+        return null;
     }
 }
