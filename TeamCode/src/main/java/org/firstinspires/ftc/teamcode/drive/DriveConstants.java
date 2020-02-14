e8ba70dd8b80feeecca161dddc3d1742b9143024
kalinkochnev <kalin.kochnev@gmail.com>
2020-02-13 21:27:32 -0500

    Committing calibration work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -35,7 +35,7 @@ public class DriveConstants {
      * MOTOR_VELO_PID with the tuned coefficients from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static final PIDCoefficients MOTOR_VELO_PID = PIDCoefficients(2, 0.5, 0);
+    public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(2, 0.5, 0);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
